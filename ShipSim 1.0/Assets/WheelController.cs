efbd512043d1472c6aec7f981f49a7182f56bf2d
fixren <2446821304@qq.com>
2023-11-14 14:04:47 +0800

    commted on 2023-11-14

---
+++ ShipSim 1.0/Assets/WheelController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour {
+
+    float ho;
+    
+	// Use this for initialization
+	void Start () {
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        ho = Input.GetAxis("Horizontal");
+        
+        transform.Rotate(0, -1 * ho, 0 );
+	}
+}
